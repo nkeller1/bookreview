b2b2c37ef334b02e8d283b59347b46da679b7b23
Nathan Keller <keller.nathan@gmail.com>
2020-08-05 12:16:13 -0600

    Create test, view and adjust controller for books index action.

--- spec/features/books/books_spec.rb
+++ spec/features/books/books_spec.rb
@@ -10,5 +10,24 @@ RSpec.describe 'books index page', type: :feature do
     expect(page).to have_content("Book List")
   end
 
-  
+  it "can see a list of all books" do
+    book1 = Book.create(
+      title: 'Words of Radiance',
+      author: 'Brandon Sanderson',
+      pages: 1200,
+      published: 2001
+    )
+    book2 = Book.create(
+      title: 'Way of Kings',
+      author: 'Brandon Sanderson',
+      pages: 1400,
+      published: 1999
+    )
+
+    visit '/books'
+
+    expect(page).to have_content('Way of Kings')
+    expect(page).to have_content('Words of Radiance')
+  end
+
 end
