2b276f54e4a2e20ae02d613907841bfa474635e0
Nathan Keller <keller.nathan@gmail.com>
2020-08-11 13:13:17 -0600

    Adjust rating spec for validation error

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :description, :rating, :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :rating }
   end
 
   describe "relationships" do
