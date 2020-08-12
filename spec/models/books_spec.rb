bbd356d2f49f2c2a9b0e7503d0dfddbeaea2718a
Nathan Keller <keller.nathan@gmail.com>
2020-08-12 11:30:55 -0600

    Create spec for gathering the three highest reviews in an array

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -81,4 +81,36 @@ RSpec.describe Book, type: :model do
 
     expect(book1.count_total_reviews).to eq(3)
   end
+
+  it ".gather_three_highest_reviews" do
+    book1 = Book.create(
+      title: 'Words of Radiance',
+      author: 'Brandon Sanderson',
+      pages: 1200,
+      published: 2001
+    )
+
+    rating1 = book1.ratings.create(
+      title: 'awesome',
+      description: 'sweet',
+      rating: 5)
+
+    rating2 = book1.ratings.create(
+      title: 'Could be Better',
+      description: 'So manhy words',
+      rating: 2)
+
+    rating3 = book1.ratings.create(
+      title: 'Cool',
+      description: 'Yay',
+      rating: 3)
+
+    rating4 = book1.ratings.create(
+      title: 'Hello',
+      description: 'test',
+      rating: 4)
+
+    expect(book1.gather_three_highest_reviews).to eq([rating1, rating3, rating4])
+  end
+
 end
