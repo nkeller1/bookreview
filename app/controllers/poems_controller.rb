4186ecde16fa1d8def95b70c93a3dae618fd21b6
Nathan Keller <keller.nathan@gmail.com>
2020-08-17 11:44:08 -0600

    Connection to poemist api established.

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,8 @@
 class PoemsController < ApplicationController
     def index
+      connection = Faraday.new "https://www.poemist.com"
+      response = connection.get('api/v1/randompoems')
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
       require "pry"; binding.pry
-      
     end
 end
