149e4564ab470334600f404350ecd8f6a7fb1c06
Nathan Keller <keller.nathan@gmail.com>
2020-08-17 11:36:21 -0600

    Create a spec to get a random poem from poemist API

---
+++ spec/features/poems/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'poems page', type: :feature do
+  it "randomly GETS a random poem" do
+
+    visit "/poems"
+
+    expect(page).to have_content('Title:')
+    expect(page).to have_content('Poem:')
+    expect(page).to have_content('Poem Url:')
+    expect(page).to have_content('Poet Name:')
+    expect(page).to have_content('Poet Url:')
+  end
+end
