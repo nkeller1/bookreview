870de0c9b06696372ff052cb35dc646a33b01c6e
Nathan Keller <keller.nathan@gmail.com>
2020-08-21 13:56:48 -0600

    Add books to show spec.

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -6,7 +6,44 @@ RSpec.describe 'books index page', type: :feature do
       title: 'Words of Radiance',
       author: 'Brandon Sanderson',
       pages: 1200,
-      published: 2001
+      published: 2001,
+    )
+
+    book2 = Book.create(
+      title: 'Oathbringer',
+      author: 'Brandon Sanderson',
+      pages: 1500,
+      published: 2004,
+    )
+    author1 = Author.create(name: 'Brandon Sanderson')
+
+    author1.books << book1
+    author1.books << book2
+
+    user = User.create(name: 'Nate',
+      email: 'example@example.com',
+      password_digest: 'hello'
+    )
+
+    rating1 = book1.ratings.create(
+      title: 'awesome',
+      description: 'sweet',
+      rating: 5,
+      user_id: user.id
+    )
+
+    rating2 = book1.ratings.create(
+      title: 'Could be Better',
+      description: 'So manhy words',
+      rating: 2,
+      user_id: user.id
+    )
+
+    rating3 = book1.ratings.create(
+      title: 'awesome',
+      description: 'sweet',
+      rating: 3,
+      user_id: user.id
     )
 
     visit "/books/#{book1.id}"
